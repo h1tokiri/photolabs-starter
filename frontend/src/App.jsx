d2d342f1782a3040ac5ffa69c7a5177d073f32e6
Javin Leslie Lau <javinlau21@gmail.com>
2025-03-16 16:48:14 -0400

    initial commit, building to display a photo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,83 @@
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
+import React from "react";
+import PhotoListItem from "./components/PhotoListItem";
+import "./App.scss";
+
+const sampleDataForPhotoListItem = {
+  id: "1",
+  username: "exampleUser ",
+  imageSource: "/Image-1-Regular.jpeg",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  profile: "/profile-1.jpg",
+};
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  const photo = {
+    id: 1,
+    urls: {
+      regular: "/Image-1-Regular.jpeg",
+    },
+    user: {
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: "/profile-1.jpg",
+    },
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+  };
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem
+        id={photo.id}
+        imageSource={photo.urls.regular}
+        username={photo.user.username}
+        city={photo.location.city}
+        country={photo.location.country}
+        profile={photo.user.profile}
+        location={photo.location}
+      />
     </div>
   );
 };
 
 export default App;
+
+/*
+import PhotoListItem from "./components/PhotoListItem";
+import "./App.scss";
+
+// Note: Rendering a single component to build components in isolation
+const App = () => {
+  // Sample data to pass as props to PhotoListItem
+  const samplePhoto = {
+    id: 1,
+    username: "exampleuser",
+    imageSource: "/Image-1-Regular.jpeg",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    profile: "/profile-1.jpg",
+  };
+
+  return (
+    <div className="App">
+      <PhotoListItem
+        id={samplePhoto.id}
+        username={samplePhoto.username}
+        imageSource={samplePhoto.imageSource}
+        location={samplePhoto.location}
+        profile={samplePhoto.profile}
+      />
+    </div>
+  );
+};
+
+export default App;
+*/
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,24 +1,49 @@
 import "../styles/PhotoListItem.scss";
 
-const sampleDataForPhotoListItem = {
-  id: 1,
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  urls: {
-    full: "/Image-1-Full.jpeg",
-    regular: "/Image-1-Regular.jpeg",
-  },
-  user: {
-    username: "exampleuser",
-    name: "Joe Example",
-    profile: "/profile-1.jpg",
-  },
-};
+// const sampleDataForPhotoListItem = {
+//   id: 1,
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   urls: {
+//     full: "/Image-1-Full.jpeg",
+//     regular: "/Image-1-Regular.jpeg",
+//   },
+//   user: {
+//     username: "exampleuser",
+//     name: "Joe Example",
+//     profile: "/profile-1.jpg",
+//   },
+// };
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({ username, imageSource, id, location, profile }) => {
+  return (
+    <div className="photo-list__item">
+      <img
+        className="photo-list__image"
+        src={imageSource}
+        alt={`Photo by ${username}`}
+      />
+
+      <div className="photo-list__user-details">
+        <img
+          className="photo-list__user-profile"
+          src={profile}
+          alt={`${username}'s profile`}
+        />
+
+        <div className="photo-list__user-info">
+          <span className="photo-list__user-name">{username}</span>
+          {location && (
+            <div className="photo-list__user-location">
+              {location.city}, {location.country}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
