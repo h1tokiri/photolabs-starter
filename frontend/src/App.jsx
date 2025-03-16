af668813469cb8f4e33b5d9e6275dca03b1a43af
Javin Leslie Lau <javinlau21@gmail.com>
2025-03-16 18:55:15 -0400

    display 3 diff photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,83 +2,96 @@ import React from "react";
 import PhotoListItem from "./components/PhotoListItem";
 import "./App.scss";
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  username: "exampleUser ",
-  imageSource: "/Image-1-Regular.jpeg",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  profile: "/profile-1.jpg",
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   username: "exampleUser ",
+//   imageSource: "/Image-1-Regular.jpeg",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   profile: "/profile-1.jpg",
+// };
 
-// Note: Rendering a single component to build components in isolation
 const App = () => {
-  const photo = {
-    id: 1,
-    urls: {
-      regular: "/Image-1-Regular.jpeg",
+  // const photo = {
+  //   id: 1,
+  //   urls: {
+  //     regular: "/Image-1-Regular.jpeg",
+  //   },
+  //   user: {
+  //     username: "exampleuser1",
+  //     name: "Joe Example",
+  //     profile: "/profile-1.jpg",
+  //   },
+  //   location: {
+  //     city: "Montreal",
+  //     country: "Canada",
+  //   },
+  // };
+
+  const photos = [
+    {
+      id: 1,
+      location: {
+        city: "Montreal",
+        country: "Canada",
+      },
+      urls: {
+        full: "/Image-1-Full.jpeg",
+        regular: "/Image-1-Regular.jpeg",
+      },
+      user: {
+        username: "exampleuser",
+        name: "Joe Example",
+        profile: "/profile-1.jpg",
+      },
     },
-    user: {
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: "/profile-1.jpg",
+
+    {
+      id: 2,
+      location: {
+        city: "Toronto",
+        country: "Canada",
+      },
+      urls: {
+        full: "/Image-2-Full.jpeg",
+        regular: "/Image-2-Regular.jpeg",
+      },
+      user: {
+        username: "exampleuser",
+        name: "Joe Example",
+        profile: "/profile-1.jpg",
+      },
     },
-    location: {
-      city: "Montreal",
-      country: "Canada",
+
+    {
+      id: 3,
+      location: {
+        city: "Ottawa",
+        country: "Canada",
+      },
+      urls: {
+        full: "/Image-3-Full.jpeg",
+        regular: "/Image-3-Regular.jpeg",
+      },
+      user: {
+        username: "exampleuser",
+        name: "Joe Example",
+        profile: "/profile-1.jpg",
+      },
     },
-  };
+  ];
 
   return (
     <div className="App">
-      <PhotoListItem
-        photo={photo}
-        // id={photo.id}
-        // imageSource={photo.urls.regular}
-        // username={photo.user.username}
-        // city={photo.location.city}
-        // country={photo.location.country}
-        // profile={photo.user.profile}
-        // location={photo.location}
-      />
+      <div className="photo-list">
+        {photos.map((photo) => (
+          <PhotoListItem key={photo.id} photo={photo} />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default App;
-
-/*
-import PhotoListItem from "./components/PhotoListItem";
-import "./App.scss";
-
-// Note: Rendering a single component to build components in isolation
-const App = () => {
-  // Sample data to pass as props to PhotoListItem
-  const samplePhoto = {
-    id: 1,
-    username: "exampleuser",
-    imageSource: "/Image-1-Regular.jpeg",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    profile: "/profile-1.jpg",
-  };
-
-  return (
-    <div className="App">
-      <PhotoListItem
-        id={samplePhoto.id}
-        username={samplePhoto.username}
-        imageSource={samplePhoto.imageSource}
-        location={samplePhoto.location}
-        profile={samplePhoto.profile}
-      />
-    </div>
-  );
-};
-
-export default App;
-*/
