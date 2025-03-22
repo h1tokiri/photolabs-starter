9ff6196c287647c586f897fc1f071f6208531391
Javin Leslie Lau <javinlau21@gmail.com>
2025-03-22 14:11:00 -0400

    close modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -6,10 +6,10 @@ const PhotoDetailsModal = ({ photo, onClose }) => {
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={onClose}>
-        {/* We'll add more content in future activities*/}
-        <img src={closeSymbol} alt="close symbol" />
+        X
+        {/* <img src={closeSymbol} alt="close symbol" />
         <h2>Photo Details</h2>
-        <p>Photo ID: {photo.id}</p>
+        <p>Photo ID: {photo.id}</p> */}
       </button>
     </div>
   );
